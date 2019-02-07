e036c0c3858e0ee413ddbe1cd853d5383d45c56c
h3ct0rjs <hfjimenez@utp.edu.co>
2019-02-06 19:17:27 -0500

    Update basic sample, debug with bash tools

--- labs/echo-client.py
+++ labs/echo-client.py
@@ -1,19 +1,25 @@
-# client 
+# client side.
 import socket
+from time import sleep
 
-RHOST = '0.0.0.0'
+
+RHOST = '0.0.0.0'   
+# Remote host and remote port.
 RPORT = 3030
 BUFFER_SIZE = 1024
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # with the  context manager with don't need to close the 
+    # connection, s.close() is automatically by the cm.
     s.connect((RHOST,RPORT))
     message = 'Hello the packet'
-    while True:
-        s.sendall(bytes(message,'utf8'))
+    for i in range(101):
+        s.sendall(bytes(message+str(i),'utf8'))
         data = s.recv(1024)
-        print(data,'\n')
-        print('Received', repr(data))
+        sleep(0.8)  
+        # delay 800ms.
+        print('Received {}'.format(repr(data)))
 
+print('Done')
 
-    s.close()
         
